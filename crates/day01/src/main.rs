278b5de5b0a45b54787d27714f014ade6973f1f3
Dan Skorupski <boardwalk@ersatsz.com>
2022-12-01 00:31:54 -0600

    day01: simplify more

--- crates/day01/src/main.rs
+++ crates/day01/src/main.rs
@@ -3,36 +3,21 @@ use itertools::Itertools as _;
 
 pub const PART1: bool = false;
 
-#[derive(Debug, Default)]
-struct Elf {
-    calories: Vec<u32>,
-}
-
 fn main() -> Result<(), Error> {
-    let elves = std::io::stdin()
+    let mut elves = std::io::stdin()
         .lines()
         .map(|line| line.unwrap())
         .group_by(|line| line.is_empty())
         .into_iter()
         .filter_map(|(is_empty, group)| if is_empty { None } else { Some(group) })
-        .map(|group| {
-            let calories = group.map(|line| line.parse().unwrap()).collect();
-            Elf { calories }
-        })
+        .map(|group| group.map(|line| line.parse::<u32>().unwrap()).sum::<u32>())
         .collect::<Vec<_>>();
 
-    // println!("{elves:?}");
+    elves.sort_unstable();
 
-    let mut total_calories: Vec<_> = elves.iter().map(|elf| elf.calories.iter().sum()).collect();
-    total_calories.sort_unstable();
-
-    if PART1 {
-        let top_calories: u32 = total_calories.iter().rev().take(1).sum();
-        println!("{top_calories}");
-    } else {
-        let top3_calories: u32 = total_calories.iter().rev().take(3).sum();
-        println!("{top3_calories}");
-    }
+    let topn = if PART1 { 1 } else { 3 };
+    let top_calories: u32 = elves.iter().rev().take(topn).sum();
+    println!("{top_calories}");
 
     Ok(())
 }
