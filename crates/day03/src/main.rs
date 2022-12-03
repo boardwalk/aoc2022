62ea057a15f20e3bac4c4634e0010f8f36dc5825
Dan Skorupski <boardwalk@ersatsz.com>
2022-12-03 01:08:46 -0600

    use bitsets

--- crates/day03/src/main.rs
+++ crates/day03/src/main.rs
@@ -1,5 +1,4 @@
 use itertools::Itertools as _;
-use std::collections::HashSet;
 
 const PART1: bool = false;
 
@@ -13,6 +12,24 @@ fn prio(b: u8) -> u32 {
     }
 }
 
+fn make_bit_set(line: &str) -> u64 {
+    line.bytes().fold(0u64, |bits, b| bits | (1 << prio(b)))
+}
+
+fn sum_bits(mut x: u64) -> u32 {
+    let mut base = 0;
+    let mut sum = 0;
+
+    while x > 0 {
+        let tz = x.trailing_zeros();
+        x >>= tz + 1;
+        base += tz;
+        sum += base;
+    }
+
+    sum
+}
+
 fn main() {
     if PART1 {
         let prio_sum = std::io::stdin()
@@ -21,11 +38,9 @@ fn main() {
                 let line = line.unwrap();
                 assert_eq!(line.len() % 2, 0);
                 let half_len = line.len() / 2;
-
-                let left = line[..half_len].bytes().collect::<HashSet<_>>();
-                let right = line[half_len..].bytes().collect::<HashSet<_>>();
-
-                left.intersection(&right).map(|b| prio(*b)).sum::<u32>()
+                let left = make_bit_set(&line[..half_len]);
+                let right = make_bit_set(&line[half_len..]);
+                sum_bits(left & right)
             })
             .sum::<u32>();
 
@@ -38,23 +53,18 @@ fn main() {
             .into_iter()
             .map(|chunk| {
                 let badges = chunk
-                    .map(|line| line.bytes().collect::<HashSet<_>>())
-                    .fold(None, |common_items: Option<HashSet<u8>>, items| {
+                    .fold(None, |common_items: Option<u64>, line: String| {
+                        let items = make_bit_set(&line);
                         if let Some(common_items) = common_items {
-                            Some(
-                                common_items
-                                    .intersection(&items)
-                                    .copied()
-                                    .collect::<HashSet<_>>(),
-                            )
+                            Some(common_items & items)
                         } else {
                             Some(items)
                         }
                     })
                     .unwrap();
 
-                assert_eq!(badges.len(), 1);
-                badges.iter().map(|b| prio(*b)).sum::<u32>()
+                assert_ne!(badges, 0);
+                sum_bits(badges)
             })
             .sum::<u32>();
 
