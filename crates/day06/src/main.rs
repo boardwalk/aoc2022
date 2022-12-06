f6b5bf818e22a9d3c61f5af21214e59586d877f9
Dan Skorupski <boardwalk@ersatsz.com>
2022-12-05 23:59:21 -0600

    day06: shorter

--- crates/day06/src/main.rs
+++ crates/day06/src/main.rs
@@ -13,19 +13,9 @@ fn main() -> Result<(), Error> {
     for line in std::io::stdin().lines() {
         let line = line?;
         let line = line.chars().collect::<Vec<_>>();
-        let mut found = false;
-        for i in marker_len..line.len() {
-            let line_part = &line[i - marker_len..i];
-            if all_different(line_part) {
-                println!("starts at {i}");
-                found = true;
-                break;
-            }
-        }
-
-        if !found {
-            println!("not found");
-        }
+        let marker_start =
+            (marker_len..line.len()).find(|&i| all_different(&line[i - marker_len..i]));
+        println!("{marker_start:?}");
     }
 
     Ok(())
