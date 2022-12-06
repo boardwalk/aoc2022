b0c37b2a50b51fa26fe73681ae5dd134f42c45fd
Dan Skorupski <boardwalk@ersatsz.com>
2022-12-06 00:15:34 -0600

    day06: bitset

--- crates/day06/src/main.rs
+++ crates/day06/src/main.rs
@@ -1,10 +1,21 @@
 use anyhow::Error;
-use std::collections::HashSet;
 
 const PART1: bool = false;
 
-fn all_different(chars: &[char]) -> bool {
-    chars.into_iter().collect::<HashSet<_>>().len() == chars.len()
+fn letter_index(ch: u8) -> u8 {
+    match ch {
+        b'a'..=b'z' => ch - b'a',
+        b'A'..=b'Z' => ch - b'A' + 26,
+        _ => panic!("invalid letter"),
+    }
+}
+
+fn all_distinct(chars: &[u8]) -> bool {
+    let num_distinct = chars
+        .iter()
+        .fold(0u64, |acc, ch| acc | (1 << letter_index(*ch)))
+        .count_ones();
+    num_distinct as usize == chars.len()
 }
 
 fn main() -> Result<(), Error> {
@@ -12,9 +23,9 @@ fn main() -> Result<(), Error> {
 
     for line in std::io::stdin().lines() {
         let line = line?;
-        let line = line.chars().collect::<Vec<_>>();
+        let line = line.as_bytes();
         let marker_start =
-            (marker_len..line.len()).find(|&i| all_different(&line[i - marker_len..i]));
+            (marker_len..line.len()).find(|&i| all_distinct(&line[i - marker_len..i]));
         println!("{marker_start:?}");
     }
 
